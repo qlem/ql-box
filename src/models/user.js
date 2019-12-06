7c376a0bedd09790c51c1d3c28286270b73fdc40
qlem <clement.dommerc@gmail.com>
2019-12-07 00:46:59 +0100

    dockerfiles prod

--- src/models/user.js
+++ src/models/user.js
@@ -3,5 +3,5 @@
 const { db } = require('./../utils/client')
 
 exports.findOne = username => db.query(`SELECT * FROM users WHERE (username=:username)`, {
-	username: username
+    username: username
 })
